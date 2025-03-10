e363c080c96a3cdb75e86e29b7d9aafc860c3d11
ThaylanFonseca <thaylanfonseca12@gmail.com>
2025-03-10 18:48:28 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.mincss">
+</head>
+<body>
+    
+
+    <footer class="rodape">
+        <div class="texto">
+            <p>&copy; 2025 Thaylan Fonseca. Todos os direitos reservados</p>
+        </div>
+        <nav class="socials">
+            <a href="https://www.facebook.com/#"> <i class="fa-brands fa-facebook"></i> </a>
+            <a href="https://www.instagram.com/#"> <i class="fa-brands fa-instagram"></i> </a>
+        </nav>
+       <button class="contact">
+            <a href="contact-us.html" class="footer-link">Fale Conosco</a>
+        </button>
+    </footer>
+</body>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="shortcut icon" href="./img/logo-site.jp.png" type="image/x-icon">
+
+</head>
+<body>
+<header class="header">
+        <nav class="indice">
+            <img src="./img/logo-site.jp.png" alt="">
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="agendamento.html">Agendamento</a></li>
+                <li><a href="horarios.html">Horarios</a></li>
+                <li><a href="ritmos.html">Ritmos</a></li>
+                <li><a href="dicas.html">Dicas</a></li>
+            </ul>
+        </nav>
+    </header>
